0d8143f748a45f878491c35e5c5b4191ecc9b579
duantrn <duantrn@gmail.com>
2022-04-02 22:46:10 +0700

    refactor the code

--- main.py
+++ main.py
@@ -1,58 +1,84 @@
 from typing import Dict
 import requests
 import locale
+import platform
+import sys
+import json
 
-API_URL = "https://api.remitano.com/api/v1/"
-LIST_CRYPTO_CURRENCIES = ["BTC", "ETH", "XRP", "LTC", "BCH", "USDT"]
-LOCALE = "vi_VN" #for windows only
-locale.setlocale(locale.LC_ALL, LOCALE)
+def loadConfig() -> Dict:
+    with open("configs.json") as f:
+        return json.load(f)
+
+def runSetings() -> None:
+    print("Running settings...")
+    # config locale 
+    if platform.system() == "Windows":
+        locale.setlocale(locale.LC_ALL, "vi_VN") # for windows only
+    else:
+        locale.setlocale(locale.LC_ALL, "vi_VN.TVCN") # for other OS
+    print("Settings end...")
 
 def getBestOffer(crypto: str) -> Dict:
-    if crypto in LIST_CRYPTO_CURRENCIES:
-        endPoint = f"{API_URL}markets/{crypto}VND/order_book"
-        r = requests.get(endPoint).json()
-        bestBid = r["bids"][0][0]
-        bestAsk = r["asks"][0][0]
-        # print(f"Best bid: {locale.currency(bestBid, grouping=True, international=True)} | Best ask: {locale.currency(bestAsk, grouping=True, international=True)}")
-        return {crypto: [bestBid, bestAsk]}
-    else:
-        print("Invalid crypto currency")
+    config = loadConfig()
+    endPoint = f"{config['API_URL']}markets/{crypto}VND/order_book"
+    r = requests.get(endPoint).json()
+    bestBid = r["bids"][0][0]
+    bestAsk = r["asks"][0][0]
+    return {crypto: [bestBid, bestAsk]}
 
-# choice: True - buy (using ask price), False - sell (using bid price)
-def offerPrice(choice: bool, crypto: str, quan: float) -> float:
+# action: True - buy (using ask price), False - sell (using bid price)
+def offerPrice(action: bool, crypto: str, quan: float) -> float:
     bestOffer = getBestOffer(crypto)
-    if choice:
-        return quan * bestOffer[crypto][1]
+    if action:
+        return quan * bestOffer[crypto][1] # buy price
     else:
-        return quan * bestOffer[crypto][0]
+        return quan * bestOffer[crypto][0] # sell price
 
 def menu() -> None:
-    print("--- OFFER FROM REMITANO ---")
-    print("1 - Show best offers")
-    print("2 - Offer price")
-    choice = input("Your choice: ")
-    print("Crypto are now available: BTC, ETH, BCH, LTC, XRP, USDT")
-    crypto = input("Your crypto currency: ")
-    if choice == "1":
-        bestOffers = getBestOffer(crypto=crypto)
-        print(f"Bid: {locale.currency(bestOffers[crypto][0], grouping=True)}")
-        print(f"Ask: {locale.currency(bestOffers[crypto][1], grouping=True)}")
-    if choice == "2":
-        action = input("Do you want to buy(1) or sell(2)? ")
-        quan = float(input("How much do you want to trade? "))
-        if action == "1":
-            buyPrice = offerPrice(choice=True, crypto=crypto, quan=quan)
-            print(locale.currency(buyPrice, grouping=True))
-        else:
-            sellPrice = offerPrice(choice=False, crypto=crypto, quan=quan)
-            print(locale.currency(sellPrice, grouping=True))
-    else:
-        print("Bye")
+    while True:
+        config = loadConfig()
+        print("--- OFFER FROM REMITANO ---")
+        print("1 - Show best offers")
+        print("2 - Offer price")
+        print("Press e to exit")
+        choice = input("Your choice: ")
+        # exit app
+        if choice == "e":
+            sys.exit("Bye")
 
+        cryptoValid = False
+        while not cryptoValid:
+            print("Crypto are now available: BTC, ETH, BCH, LTC, XRP, USDT")
+            crypto = input("Your crypto currency: ")
+            if crypto not in config["LIST_CRYPTO_CURRENCIES"]:
+                print("Invalid crypto")
+            else:
+                cryptoValid = True
+        if choice == "1":
+            bestOffers = getBestOffer(crypto=crypto)
+            print(f"Bid: {locale.currency(bestOffers[crypto][0], grouping=True)}")
+            print(f"Ask: {locale.currency(bestOffers[crypto][1], grouping=True)}")
+        elif choice == "2":
+            actionValid = False
+            while not actionValid:
+                try: 
+                    action = int(input("Do you want to buy(1) or sell(0)? "))
+                    if action < 0 or action > 1:
+                        print("Must be 0 or 1...")
+                    else:
+                        action = bool(action)
+                        actionValid = True
+                except ValueError:
+                    print("Must be integer...")
+            quan = float(input("How much do you want to trade? "))
+            price = offerPrice(action=action, crypto=crypto, quan=quan)
+            print(locale.currency(price, grouping=True))
+        
+
+def run() -> None:
+    runSetings()
+    menu()
 
 if __name__ == "__main__":
-    # print("running")
-    # print(getBestOffer("BTC"))
-    # offerPrice = locale.currency(offerPrice(True, "BTC", 1), grouping=True)
-    # print(offerPrice)
-    menu()
+    run()
+    
